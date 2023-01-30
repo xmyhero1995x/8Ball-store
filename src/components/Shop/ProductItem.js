27c34f7aafead0c519f716b64a6118a60aff8915
xmyhero1995x <xmyhero95x@gmail.com>
2023-01-30 21:49:55 +0200

    added  new reducer 'updateCart'

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,20 +1,44 @@
 import Card from "../UI/Card";
 import styles from "./ProductItem.module.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { cartSliceActions } from "../../store/cart-slice";
 
 const ProductItem = (props) => {
+  const cart = useSelector((state) => state.cart);
   const disputchFunction = useDispatch();
 
   const { title, price, description, id } = props;
 
   const addItemHandler = () => {
-    disputchFunction(
-      cartSliceActions.addItem({
-        id,
-        title,
-        price,
+    const updatedItemsQuantity = cart.itemsQuantity + 1;
+
+    const updatedItems = cart.items.slice();
+    const existingItem = updatedItems.find((item) => item.id === id);
+
+    if(existingItem) {
+      const updatedExistingItem = { ...existingItem};
+      updatedExistingItem.quantity++;
+      updatedExistingItem.totalPrice =  updatedExistingItem.totalPrice + price;
+
+      const existingItemIndex = updatedItems.findIndex((item) => item.id === id);
+      updatedItems[existingItemIndex] = updatedExistingItem;
+    } else {
+      updatedItems.push({
+        id: id,
+        price: price,
+        quantity: 1,
+        totalPrice: price,
+        title: title,
       })
+    }
+
+
+    const updatedCart = {
+      itemsQuantity: updatedItemsQuantity,
+      items: updatedItems,
+    }
+    disputchFunction(
+      cartSliceActions.updateCart(updatedCart)
     );
   };
   return (
